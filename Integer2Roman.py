13a0cff15cee430d32b222698519bb6e4d1af724
chaulagainrupan1 <rupanchaulagain.rc@gmail.com>
2020-12-23 16:18:24 +0545

    bug fix

--- Integer2Roman.py
+++ Integer2Roman.py
@@ -29,4 +29,4 @@ if __name__ == "__main__":
     if(integerNumber <= 0):
         print("Invalid input")
     else:
-        IntegertoRoman(integerNumber)
+        print(IntegertoRoman(integerNumber))
--- test_Integer2Roman.py
+++ test_Integer2Roman.py
@@ -1,8 +1,6 @@
 import unittest
 import Integer2Roman
 
-print(Integer2Roman.IntegertoRoman(1994))
-
 class TestInteger2Roman(unittest.TestCase):
     def test_integertoRoman(self):
         integer = 1994
